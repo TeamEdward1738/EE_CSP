9c05ff9e708e9122c344e7728f333098fc86059b
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-09 13:52:12 -0600

    comm

---
+++ varibles_notes.py
@@ -0,0 +1,7 @@
+# EE, Variables Notes
+
+name = input("tell me your name: treyson ")
+
+print("csp is the worst class")
+print ("it is taught with a cool teacher")
+print("Hello", name, "welsome to class!" )
