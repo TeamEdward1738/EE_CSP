0350e13b8012757b8b9f55f02df4f98ba5ecad9a
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-10 13:50:16 -0600

    Variables practice

---
+++ Variable_Practicce.py
@@ -0,0 +1,23 @@
+# EE, variable practice
+
+name = "kylie"
+five = "5"
+big_number = "353"
+breakfast = "pumpkin chocolate cookie"
+favorite_color = "lavender"
+school_name = "american preparatory academy"
+year = "2026"
+I_color = "grayish green-blue"
+age ="67" 
+subject = "math"
+
+print(name)
+print(five)
+print(big_number)
+print(breakfast)
+print(favorite_color)
+print(school_name)
+print(year)
+print(I_color)
+print(age)
+print(subject)
