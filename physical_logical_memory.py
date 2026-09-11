c10a172bf933c0c3e53c83635606ffcc51b5ef08
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-11 13:35:41 -0600

    physical_logical_memory

---
+++ physical_logical_memory.py
@@ -0,0 +1,9 @@
+# EE, Physical and logical memory
+
+pi = 3.1415
+
+print(pi)
+print(id(pi))
+everything = 42
+print(f"the variable pi is {pi} and it is saved at {id(pi)}")
+print(f"the variable everything is {everything} and it is saved at {id(everything)}")
