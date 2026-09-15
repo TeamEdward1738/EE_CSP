50202c367bb64f1b119e19bf32d481c4871847a0
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-15 13:29:29 -0600

    fixing_inputs

---
+++ fixing_inputs.py
@@ -0,0 +1,22 @@
+# EE, Fixing Inputs
+
+# When you want a specific input
+while True:
+    color = input("Tell me a color that is only one word: ").lower().strip()
+    if color.isnumeric():
+        print("sorry that is a number")
+    elif "" in color: 
+        print("I said one word")
+    else:
+        break
+print(f"I painted your {color}!")
+
+# when you want number
+while True:
+     try:
+         age = int(input("How old are you: "))
+         break
+     except:
+         print("That isn't a number")
+
+print(f"Wow you are {age} that is really old!")
