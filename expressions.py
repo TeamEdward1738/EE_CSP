c5f94de38ed0b88d64e2a1236210185189c0c16a
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-16 13:51:11 -0600

    expressions notes

---
+++ expressions.py
@@ -0,0 +1,28 @@
+# EE Integers, Floats and expressions nots
+
+# Integers
+people = 18
+
+shoes = people * 2
+
+hand = people * 2
+
+# Float 
+pi  = 3.141592653589793
+
+water = 60.5 
+
+temp = 68.5
+
+price = 1.99
+
+# expression 
+print(f"Your {water} gallons of water cost ${round(water*price,2)}")
+
+
+# modulo and interger division
+print(f"18/4= {18/4} which is {18//4} with a remainder of {18%4}")
+print(f"18/5= {18/5} which is {18//5} with a remainder of {18%5}")
+print(f"18/6= {18/6} which is {18//6} with a remainder of {18%6}")
+
+print(int(people*2*(5-1)/3))
