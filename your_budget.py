eb2378ce7c89db459d2e3d0a744e5aa673476038
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-17 13:50:49 -0600

    your_budget

---
+++ your_budget.py
@@ -0,0 +1,12 @@
+# EE, your_budget
+
+income_monthly= int(input("tell me your monthly income: $3000"))
+
+rent_monthly= int(input("tell me your monthly rent: $1200"))
+
+utilities_monthly= int(input("tell me your monthly utilities: $200"))
+
+groceries_monthly= int(input("tell me your monthly groceries: $250"))
+
+transportation_monthly= int(input("tell me your monthly transportation: $500"))
+
