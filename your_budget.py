6956689a19f9f7cc8766a2bd5d733436cdd3454b
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-18 16:37:38 -0600

    your_budget

--- your_budget.py
+++ your_budget.py
@@ -1,12 +1,22 @@
 # EE, your_budget
 
-income_monthly= int(input("tell me your monthly income: $3000"))
+income_monthly= float(input("tell me your monthly income: $"))
 
-rent_monthly= int(input("tell me your monthly rent: $1200"))
+rent_monthly= float(input("tell me your monthly rent: $"))
 
-utilities_monthly= int(input("tell me your monthly utilities: $200"))
+utilities_monthly= float(input("tell me your monthly utilities: $"))
 
-groceries_monthly= int(input("tell me your monthly groceries: $250"))
+groceries_monthly= float(input("tell me your monthly groceries: $"))
 
-transportation_monthly= int(input("tell me your monthly transportation: $500"))
+transportation_monthly= float(input("tell me your monthly transportation: $"))
 
+print(f"Your rent is ${rent_monthly:.2f} and that is {int(round(rent_monthly/income_monthly*100))}% of your income")
+
+print(f"Your utilities bill is ${utilities_monthly:.2f} and that is {int(round(utilities_monthly/income_monthly*100))}% of your income")
+
+print(f"Your groceries bill is ${groceries_monthly:.2f} and that is {int(round(groceries_monthly/income_monthly*100))}% of your income")
+
+print(f"Your transportation bill is ${transportation_monthly:.2f} and that is {int(round(transportation_monthly/income_monthly*100))}% of your income")
+
+print(f"You save ${income_monthly*0.1:.2f} a month and that is 10% of your income")
+print(f"You have $550.00 of spending money each month")
