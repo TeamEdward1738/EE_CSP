9908c3c1f1d35de03d2ba14598f6d5b893baa02c
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-21 13:52:05 -0600

    silly sentences

--- silly_sentences.py
+++ silly_sentences.py
@@ -1,2 +1,11 @@
 # EE, 6th silly_sentences
 
+article_of_clothing= (input("give me an article of clothing"))
+
+shape_ending_in_s= (input("give me a shape ending in s"))
+
+emotion= (input("give me an emotion"))
+
+number=(input("give me a number"))
+
+smaller_number=(input("give me a smaller number"))
