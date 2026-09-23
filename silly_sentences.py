9dd767b0deaad886dfdd6ec719a097d3ff3e2071
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-23 13:15:40 -0600

    silly sentences

--- silly_sentences.py
+++ silly_sentences.py
@@ -9,3 +9,19 @@ emotion= (input("give me an emotion"))
 number=(input("give me a number"))
 
 smaller_number=(input("give me a smaller number"))
+
+favorite_character=(input("tell me your favorite character"))
+
+verb=(input("give me a verb"))
+
+NUMBER=(input("give me a numbers starting with 1 and ending with 0"))
+
+birthday=(input("tell me your birthday"))
+
+color=(input("give me a color"))
+
+adjective=(input("give me an adjective"))
+
+
+
+
