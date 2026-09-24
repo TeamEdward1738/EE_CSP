be8a3d981ae6ba16454a507103a858ad3178c12d
TeamEdward1738 <elsie.ewell@ucas-edu.net>
2026-09-24 13:15:09 -0600

    silly sentences

--- silly_sentences.py
+++ silly_sentences.py
@@ -1,35 +1,35 @@
 # EE, 6th silly_sentences
 
-article_of_clothing= (input("give me an article of clothing"))
+article_of_clothing= input("give me an article of clothing:")
 
-shape_ending_in_s= (input("give me a shape ending in s"))
+shape_ending_in_s= input("give me a shape ending in s:")
 
-emotion= (input("give me an emotion"))
+emotion= input("give me an emotion:")
 
-number=(input("give me a number"))
+number=input("give me a number:")
 
-smaller_number=(input("give me a smaller number"))
+smaller_number=input("give me a smaller number:")
 
-favorite_character=(input("tell me your favorite character"))
+favorite_character=input("tell me your favorite character:")
 
-verb=(input("give me a verb"))
+verb=input("give me a verb:")
 
-Number=(input("give me a Number starting with 1 and ending with 0"))
+Number=input("give me a Number starting with 1 and ending with 0:")
 
-birthday=(input("tell me your birthday"))
+birthday=input("tell me your birthday:")
 
-color=(input("give me a color"))
+color=input("give me a color:")
 
-adjective=(input("give me an adjective"))
+adjective=input("give me an adjective:")
 
-another_verb=(input("give me another verb"))
+another_verb=input("give me another verb:")
 
-body_part=(input("give me a body part"))
+body_part=input("give me a body part:")
 
-type_of_food(input("give me a type of food"))
+type_of_food=input("give me a type of food:")
 
-part_of_a_spaceship(input("give me a part of a spaceship"))
+part_of_a_spaceship=input("give me a part of a spaceship:")
 
-type_of_job(input("give me a type of job"))
+type_of_job=input("give me a type of job:")
 
-
+print(f"I snapped on my {article_of_clothing} covered in {shape_ending_in_s}.{emotion}.I counted down from {number} to {smaller_number}. It was time for my spaceship,the {favorite_character} to {verb}!{Number} {birthday} blastoff! Soon I could see the {color} holes in the moon.It was so{adjective}.I {another_verb} up for a closer look. People think there is a face in the moon, but I saw a {body_part}. Others think the moon is made of cheese but really it's made of {type_of_food}. I opened the {part_of_a_spaceship} and became the first {type_of_job}on the moon.")
